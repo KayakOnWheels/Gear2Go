e4671c4c467d049fccd27f050e7685fb10ae4ebe
KayakOnWheels <115392859+KayakOnWheels@users.noreply.github.com>
2024-06-24 17:58:13 +0200

    Created basic project structure

---
+++ src/main/java/com/gear2go/Gear2GoApplication.java
@@ -0,0 +1,13 @@
+package com.gear2go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gear2GoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gear2GoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gear2go/Gear2GoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gear2go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gear2GoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
