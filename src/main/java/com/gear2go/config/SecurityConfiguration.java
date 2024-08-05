a803fcc4e7e2543fc51dc6430e9a5386d54bc31b
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-08-05 16:41:55 +0200

    Corrected securityFilterChain endpoints

--- src/main/java/com/gear2go/config/SecurityConfiguration.java
+++ src/main/java/com/gear2go/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,52 +23,47 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-/*        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-    return http.build();*/
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
 
-                        .requestMatchers(   //role = ADMIN
-                                "v1/*/admin/**",
-                                "v1/*/admin",
-                                "v1/product/crud",
-                                "v1/*/admin/**"
-                        ).hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.POST, "v1/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/auth/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/auth//authenticate-guest").permitAll()
+                        .requestMatchers(HttpMethod.GET, "v1/product").permitAll()
+                        .requestMatchers(HttpMethod.GET, "v1/product/*").permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/product/availability").permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/user/request-recovery").permitAll()
+                        .requestMatchers(HttpMethod.POST, "v1/user/register").permitAll()
 
-                        .requestMatchers(   //permitAll
-                                "v1/auth/**",
-                                "v1/weather/**",
-                                "v1/auth/authenticate/guest",
-                                "v1/user/request-recovery",
-                                "v1/product/**",
-                                "v1/user/**",
-                                "v1/product/availability",
-                                "v1/cart/**",
-                                "actuator/mappings"
-                        ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "v1/address").authenticated()
+                        .requestMatchers(HttpMethod.POST, "v1/address").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "v1/address").authenticated()
+                        .requestMatchers(HttpMethod.POST, "v1/auth/recover-password").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "v1/cart/dates").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "v1/cart").authenticated()
+                        .requestMatchers(HttpMethod.POST, "v1/cart").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "v1/user/update").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "v1/user").authenticated()
+
+                        .requestMatchers(HttpMethod.GET, "v1/address/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.DELETE, "v1/address/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.GET, "v1/cart/all").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.GET, "v1/order/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.DELETE, "v1/order/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.POST, "v1/product/add").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.PUT, "v1/product/update").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.DELETE, "v1/product/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.GET, "v1/user").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.GET, "v1/user/*").hasRole(String.valueOf(Role.ADMIN))
+                        .requestMatchers(HttpMethod.DELETE, "v1/user/*").hasRole(String.valueOf(Role.ADMIN))
 
                         .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()   //actuator endpoints
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //.authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
